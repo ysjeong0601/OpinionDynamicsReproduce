32557b4ead0c577d819a9f306c6f612dd5b4b90d
jeong-yeonsu <yeonsu0601@hanyang.ac.kr>
2021-07-26 09:37:57 +0900

    Initial Commit

---
+++ OpinionDynamics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpinionDynamics
+//
+//  Created by 정연수 on 2021/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
